16398c83844b31c66b81fe14e931f00b22e52dfb
dianerdiana <dianerdiana.de@gmail.com>
2026-04-11 21:26:56 +0700

    fix(eslint): disable no-unsafe-call rule in TypeScript ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -82,6 +82,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
       'prettier/prettier': ['warn', { endOfLine: 'auto' }],
     },
   },
